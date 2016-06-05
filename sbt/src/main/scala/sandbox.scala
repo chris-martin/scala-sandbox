2d6406062611383de321d4a057d724e3c299b02d
Chris Martin <ch.martin@gmail.com>
2016-06-05 17:07:40 -0400

    underlying

--- sbt/src/main/scala/sandbox.scala
+++ sbt/src/main/scala/sandbox.scala
@@ -1,38 +1,32 @@
-import java.util.concurrent.TimeUnit
-
-import sandbox.Handle.Listener
-
-import scala.concurrent.Future
 
 package object sandbox {
 
-  def unblock[A](x: => Handle[A]): Handle[A] =
-    new Handle[A] {
+  sealed trait Number {
+    type A
+    def underlying: A
+    def + (num : Number) : Number = ???
+    def - (num : Number) : Number = ???
+    def * (num : Number) : Number = ???
+  }
 
-      private[this] val lock = new Object()
+  sealed trait SignedNumber extends Number
 
-      val listeners = collection.mutable.ArrayBuffer[Listener[A]]()
-      var handleOption: Option[Handle[A]] = None
+  sealed trait UnsignedNumber extends Number
 
-      Future(x).onSuccess {
-        case h => lock.synchronized {
-          handleOption = Some(h)
-          listeners.foreach(h.addListener)
-        }
-      }
+  sealed trait UInt32 extends UnsignedNumber {
+    override type A = Long
+  }
 
-      override def addListener(l: Listener[A]): Unit = lock.synchronized {
-        handleOption.fold(listeners.append(l))(_.addListener(l))
-      }
+  sealed trait UInt64 extends UnsignedNumber {
+    override type A = BigInt
+  }
 
-      override def isCancelled: Boolean = lock.synchronized {
-        handleOption.fold(listeners.append(l))(_.addListener(l))
-      }
+  sealed trait Int32 extends SignedNumber {
+    override type A = Int
+  }
 
-      override def get(): A = ???
-      override def get(timeout: Long, unit: TimeUnit): A = ???
-      override def cancel(mayInterruptIfRunning: Boolean): Boolean = ???
-      override def isDone: Boolean = ???
-    }
+  sealed trait Int64 extends SignedNumber {
+    override type A = Long
+  }
 
 }
